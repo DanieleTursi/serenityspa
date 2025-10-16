e350f5befd4e09d8a3a05990dc86ddd8571f717e
Daniele Tursi <tursidaniele@gmail.com>
2025-10-16 15:26:10 +0100

    loading bug fixed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,32 +1,30 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* Preload the hero image for faster first paint */}
-        <link rel="preload" href="/images/serenity-hero.jpg" as="image" />
-        {/* Hint browser to fetch important images earlier */}
-        <link rel="preload" href="/images/spa1.jpg" as="image" />
-      </Head>
-      <body>
-        {/* Hidden eager image to ensure the hero image starts downloading immediately in all browsers */}
-        <img
-          src="/images/serenity-hero.jpg"
-          alt=""
-          style={{
-            position: "absolute",
-            width: "1px",
-            height: "1px",
-            opacity: 0,
-            pointerEvents: "none",
-          }}
-          loading="eager"
-          fetchpriority="high"
-        />
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
